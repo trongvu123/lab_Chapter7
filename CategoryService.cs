e3ac33d9f4f7ea70b2a17a5da708a34805773b68
Vu Nguyen <vunguyen2300@gmail.com>
2024-06-05 23:05:58 +0700

    Lab chapter7

---
+++ CategoryService.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabMyStoreWPF
+{
+    public class CategoryService
+    {
+        private readonly string _connectionString= "Data Source=localhost,1433;Initial Catalog=MyStore;User ID=sa;Password=123456;TrustServerCertificate=True";
+
+        public CategoryService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            var categories = new List<Category>();
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = @"SELECT [CategoryID]
+      ,[CategoryName]
+  FROM [dbo].[Categories]
+";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            categories.Add(new Category
+                            {
+                                Id = (int)reader["CategoryID"],
+                                Name = reader["CategoryName"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            return categories;
+        }
+
+        public void AddCategory(Category category)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = @"INSERT INTO [dbo].[Categories]([CategoryName]) VALUES (@Name)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Name", category.Name);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE [dbo].[Categories] SET [CategoryName] = @Name WHERE CategoryID = @Id";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", category.Id);
+                    cmd.Parameters.AddWithValue("@Name", category.Name);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteCategory(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM [dbo].[Categories] WHERE CategoryID = @Id";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabMyStoreWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CategoryService _categoryService;
+        private List<Category> _categories;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = "Data Source=localhost,1433;Initial Catalog=MyStore;User ID=sa;Password=123456;TrustServerCertificate=True";
+            _categoryService = new CategoryService(connectionString);
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            _categories = _categoryService.GetAllCategories();
+            CategoriesDataGrid.ItemsSource = _categories;
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(name))
+            {
+                var newCategory = new Category { Name = name };
+                _categoryService.AddCategory(newCategory);
+                LoadCategories();
+                NameTextBox.Clear(); // Clear the TextBox after adding
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid name.");
+            }
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            if (CategoriesDataGrid.SelectedItem is Category selectedCategory)
+            {
+                string name = NameTextBox.Text.Trim();
+                if (!string.IsNullOrEmpty(name))
+                {
+                    selectedCategory.Name = name;
+                    _categoryService.UpdateCategory(selectedCategory);
+                    LoadCategories();
+                    NameTextBox.Clear(); // Clear the TextBox after updating
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid name.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a category to update.");
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (CategoriesDataGrid.SelectedItem is Category selectedCategory)
+            {
+                _categoryService.DeleteCategory(selectedCategory.Id);
+                LoadCategories();
+            }
+            else
+            {
+                MessageBox.Show("Please select a category to delete.");
+            }
+        }
+    }
+}
